9678a25f8fe944d10282ddfb319641ca71c570f4
Cindy Qu <cindyqu42@gmail.com>
2022-10-23 15:30:35 -0700

    updated routes and minimal css

--- src/components/SongCollection/SongCollection.js
+++ src/components/SongCollection/SongCollection.js
@@ -1,7 +1,25 @@
-import 'SongCollection.css'
+import './SongCollection.css';
+import React from 'react';
+import {Link} from 'react-router-dom'
 
-function SongCollection(){
 
+function SongCollection () {
 
+    return (
+        <div className='songcollection'>
+     
+        <h1>Collection</h1>
+       
+        <p>Killer Queen<button><Link to="/songdisplay"> Click to see song detail</Link></button></p> 
+        
+    
+        
+      <button><Link to="/songentry"> Add A New Song</Link></button>
+     
+    </div>
+    )
     
 }
+
+
+export default SongCollection;
--- src/components/SongDisplay/SongDisplay.js
+++ src/components/SongDisplay/SongDisplay.js
@@ -1,7 +1,26 @@
-import 'SongDisplay.css'
+import './SongDisplay.css';
+import React from 'react';
+import {Link} from 'react-router-dom'
 
-function SongDisplay(){
+const SongDisplay = () => {
+    return (
+        <div className='songdisplay'>
+     
+        <h1>Song Display</h1>
+        <p>Title</p>
+        <p>Artist</p>
+        <p>Genre</p>
+        <p>Rating</p>
+        
 
+        <button><Link to="/songcollection"> Back to Song Collection</Link></button>
 
+        
+     
+    </div>
+    )
     
 }
+
+
+export default SongDisplay;
