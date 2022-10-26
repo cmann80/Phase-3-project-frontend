bf30ac372721927c44b1b0c52ef63bbd825d9742
Chris Mann <chrispmann@gmail.com>
2022-10-26 11:49:54 -0700

    cleaned up indents etc

--- src/components/SongDisplay/SongDisplay.js
+++ src/components/SongDisplay/SongDisplay.js
@@ -48,18 +48,16 @@ function handleSubmitUpdate(e){
         <p>Title: {fullSong?.song?.title}</p>
         <p>Artist: {fullSong?.song?.artist}</p>
         <p>Genre: {fullSong?.song?.genre}</p>
-        <p>Your Rating: {fullSong?.song_rating}   <select name="newrating" value={newSongRating} 
-            onChange={(e) => setNewSongRating(parseInt(e.target.value)) }>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-</select>
-            
-            </p>
-
-      
+        <p>Your Rating: {fullSong?.song_rating}   
+            <select name="newrating" value={newSongRating} 
+                onChange={(e) => setNewSongRating(parseInt(e.target.value)) }>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+            </select>
+        </p>
 
         <p>{avgSong}</p>
         
